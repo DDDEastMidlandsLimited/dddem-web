aab5dd3507ee84ddd36113c7ad0bf92b438f0ae3
Jessica White <me@jesswhite.co.uk>
2018-12-08 11:57:55 +0000

    Edit teh text on Venue page
    ToDo:
    - Add image

--- pages/venue.js
+++ pages/venue.js
@@ -16,9 +16,15 @@ export default (props) => (
         <Header title={'Venue'}/>
 
         <section>
-            <h2>
+            <h1>
+                We are super excited to announce that we will be hosted by <a href="http://www.nottinghamconferencecentre.co.uk/">The Nottingham Conference Centre</a>.
+            </h1>
+        </section>
+
+        <section>
+            <h1>
                 Directions to Nottingham Conference Centre
-            </h2>
+            </h1>
         </section>
 
         <section>
@@ -54,42 +60,47 @@ export default (props) => (
             </h2>
             <p>
                 From the north, exit the M1 at junction 26 and follow the signs for the A610 towards Nottingham city
-                centre. There is a Park and Ride (tram) facility situated close to the M1 junction 26. The Park and Ride
-                (Phoenix Park) site is clearly sign-posted off the A610. Leave the tram at the Nottingham Trent
-                University tram stop which is located on Goldsmith Street and walk past the main University entrance,
-                turn left on to Burton Street where you will find the Nottingham Conference Centre entrance. From the
-                south, exit the M1 at junction 24 and follow the signs for the A453 to Nottingham city centre. The
-                Queen’s Drive Park and Ride is located just off the A453 (Queen’s Drive), follow signs for A453 /
+                centre.
+            </p>
+            <p>
+                There is a Park and Ride (tram) facility situated close to the M1 junction 26. The Park and Ride
+                (Phoenix Park) site is clearly sign-posted off the A610. 
+            </p>
+            <p>
+                Leave the tram at the Nottingham Trent University tram stop which is located on Goldsmith Street and walk past the main University entrance,
+                turn left on to Burton Street where you will find the Nottingham Conference Centre entrance. 
+            </p>
+            <p>
+                From the south, exit the M1 at junction 24 and follow the signs for the A453 to Nottingham city centre. 
+            </p>
+            <p>
+                The Queen’s Drive Park and Ride is located just off the A453 (Queen’s Drive), follow signs for A453 /
                 Queen’s Drive Industrial Estate and merge on to the A453 (Queen’s Drive). The Park and Ride is located
-                on the opposite side of the road to the retail park and is clearly sign-posted. Alight from the Park and
-                Ride bus on Lower Parliament Street (Victoria Centre); Nottingham Conference Centre is a short walk
+                on the opposite side of the road to the retail park and is clearly sign-posted. 
+            </p>
+            <p>
+                Alight from the Park and Ride bus on Lower Parliament Street (Victoria Centre); Nottingham Conference Centre is a short walk
                 away.
             </p>
         </section>
 
         <section>
-            <h2>
+            <strong>
                 Car parking
-            </h2>
+            </strong>
             <p>
                 Car parks in the city centre are clearly signposted from all major approach routes. There are two car
                 parks close to Nottingham Conference Centre, Trinity Square car park on North Church Street and Talbot
                 Street car park. For satellite navigation systems please use the following information:
             </p>
             <p>
-                <strong>Trinity Square car park:</strong> postcode NG1 4BR<br />
+                <strong><a href="http://www.nottinghamcity.gov.uk/transport-parking-and-streets/parking-and-permits/city-centre-parking/car-parks/trinity-square-car-park/" target="_blank">Trinity Square car park:</a></strong> postcode NG1 4BR<br />
                 co-ordinates 52.956785,-1.149316
             </p>
             <p>
-                <strong>Talbot Street car park:</strong> postcode NG1 5GG<br />
+                <strong><a href="https://www.q-park.co.uk/en-gb/cities/nottingham/talbot-street/" target="_blank">Talbot Street car park:</a></strong> postcode NG1 5GG<br />
                 co-ordinates 52.956143,-1.154433
             </p>
-        </section>
-
-
-        <section>
-            <h2>Important Dates</h2>
-            <ImportantDatesList dates={dates}/>
         </section>
 
         <style jsx>
