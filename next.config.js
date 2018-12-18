4477189fbf998fafd0322de347e84775944444a8
John Nolan <johnnolan@nolanscafe.co.uk>
2018-12-18 17:20:40 +0000

    Favicon (#8)
    * Updated manifest file to fix favicons and contain descriptive
    attributes
    
    * Added static pages to nextjs export config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     exportPathMap: function () {
         return {
-            '/': { page: '/' }
+            '/': { page: '/' },
+            '/about/': { page: '/about' },
+            '/venue/': { page: '/venue' },
+            '/sponsorship/': { page: '/sponsorship' },
+            '/faq/': { page: '/faq' },
+            '/code-of-conduct/': { page: '/code-of-conduct' }
         }
     }
 }
