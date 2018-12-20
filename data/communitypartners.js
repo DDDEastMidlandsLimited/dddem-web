580f96f26e9a5b6632b85a81c4f825f7a1054c27
Jessica White <me@jesswhite.co.uk>
2018-12-20 17:12:18 +0000

    Add Tech Nott details

--- data/communitypartners.js
+++ data/communitypartners.js
@@ -1,5 +1,11 @@
 const compartners = [
-    { id: 1, name: 'Partner 1', image: '/static/partners/technott.png', link: '', description: '' },
+    { 
+        id: 1, 
+        name: 'Tech Nottingham', 
+        image: '/static/partners/technott.png', 
+        link: '', 
+        description: '' 
+    },
     { id: 2, name: 'Partner 2', image: '', link: '', description: '' },
 ];
 
