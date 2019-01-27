7b920d6b053b967694fa6dbc4f4408e5e5c6094b
Jessica White <me@jesswhite.co.uk>
2019-01-27 15:24:05 +0000

    Update sponsorship page

--- data/extraopportunities.js
+++ data/extraopportunities.js
@@ -1,8 +1,8 @@
 const extraopportunities = [
-    { id: 5, name: 'Inclusivity Sponsor', soldOutImage: null, description: 'Available' },
-    { id: 6, name: 'Workshop Sponsor', soldOutImage: '/static/soldout/workshopsold.png', description: 'Needs Sold Out Image' },
-    { id: 7, name: 'Coffee Sponsor', soldOutImage: '/static/soldout/coffeesold.png', description: 'Needs Sold Out Image' },
-    { id: 8, name: 'Booklet Sponsor', soldOutImage: null, description: 'Available' },
+    { id: 5, name: 'Workshop Sponsor', soldOutImage: '/static/soldout/workshopsold.png', description: 'Sold Out' },
+    { id: 6, name: 'Coffee Sponsor', soldOutImage: '/static/soldout/coffeesold.png', description: 'Sold Out' },
+    { id: 7, name: 'Booklet Sponsor', soldOutImage: '/static/soldout/bookletsold.png', description: 'Sold Out' },
+    { id: 8, name: 'Inclusivity Sponsor', soldOutImage: null, description: 'Available' },
     { id: 9, name: 'Bathroom Basket', soldOutImage: null, description: 'Available' },
     { id: 10, name: 'Notable Company Logo', soldOutImage: null, description: 'Available' }
 ];
--- data/sponsors.js
+++ data/sponsors.js
@@ -1,7 +1,7 @@
 const sponsors = [
     { id: 1, name: 'Gold Tier', soldOutImage: '/static/soldout/goldsold.png', description: '' },
-    { id: 2, name: 'Silver', soldOutImage: null, description: 'Two Remaining' },
-    { id: 3, name: 'Bronze', soldOutImage: null, description: 'Four Remaining' },
+    { id: 2, name: 'Silver', soldOutImage: null, description: 'One Remaining' },
+    { id: 3, name: 'Bronze', soldOutImage: null, description: 'Three Remaining' },
     { id: 4, name: 'Standard', soldOutImage: null, description: 'Unlimited' }
 ];
 
