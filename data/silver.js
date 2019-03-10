6f25e4db3a641a73bad75f19acebdcd297f795af
Jessica White <code.jessw@gmail.com>
2019-03-10 14:24:09 +0000

    Add UNiDAYS logo to homepage and Notable Companies

--- data/silver.js
+++ data/silver.js
@@ -1,3 +1,7 @@
-const silver = [];
+const unidaysDesc = '<h1>UNiDAYS.</h1><p>The world\'s leading Student Affinity Network with access to millions of college and university students around the world. We connect brands with Gen Z all in one platform</p><a href="https://corporate.myunidays.com/en-gb/" target="_blank" category="notable-companies" eventType="UNiDAYS">Learn More</a>'
+
+const silver = [
+    { id: 1, name: 'UNiDAYS', image: '/static/sponsors/UNiDAYS_200.png', link: 'https://www.myunidays.com/GB/en-GB/careers', description: unidaysDesc },
+];
 
 export default silver;
