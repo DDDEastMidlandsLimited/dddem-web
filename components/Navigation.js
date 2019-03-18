4cf2203bca818226caa793becc64469909801fc0
Jessica White <me@jesswhite.co.uk>
2019-03-18 15:06:40 +0000

    Enhancement: Ticket button on navigation and homepage
    * Link to eventbrite on nav
    
    * Button on index page

--- components/Navigation.js
+++ components/Navigation.js
@@ -29,14 +29,21 @@ class Navigation extends Component {
             <NavDropdown.Item href="/sponsorship">Sponsorship</NavDropdown.Item>
           </NavDropdown>
           <Nav.Link href="/faq">FAQ</Nav.Link>
+          
           <Nav.Link target="_blank" href="https://sessionize.com/dddeastmidlands/"
             onSelect={event => {
               logEvent("navigation", "cfp")
             }}>CFP</Nav.Link>
-            <Nav.Link target="_blank" href="https://medium.com/dddeastmidlands"
+          
+          <Nav.Link target="_blank" href="https://medium.com/dddeastmidlands"
             onSelect={event => {
               logEvent("navigation", "blog")
             }}>Blog</Nav.Link>
+
+          <Nav.Link target="_blank" href="https://www.eventbrite.co.uk/e/ddd-east-midlands-tickets-58629047058"
+            onSelect={event => {
+              logEvent("navigation", "eventbrite")
+            }}>Tickets</Nav.Link>
 
           <Nav.Link href="/code-of-conduct">Code of Conduct</Nav.Link>
         </Nav>
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import dates from "../data/dates";
 import gold from "../data/gold";
 import silver from "../data/silver";
 import bronze from "../data/bronze";
+import TicketButton from '../components/TicketButton';
 import ImportantDatesList from "../components/ImportantDatesList";
 import TieredCompanies from '../components/TieredCompanies';
 
@@ -16,6 +17,10 @@ export default () => (
             <title>DDD East Midlands</title>
         </Head>
         <Header title={'DDD East Midlands'}/>
+
+
+        <h2 className="ticket-header">First Wave Of Tickets Go Live: Saturday 23rd March at 9am!</h2>
+        <TicketButton/>
 
         <section>
             <p>
