033755c6e02d2dfa1a5e4ecd0198edcfcb516f01
Jessica White <code.jessw@gmail.com>
2019-03-27 20:17:26 +0000

    Remove ticket button and NDC Conf desc

--- data/standard.js
+++ data/standard.js
@@ -1,7 +1,7 @@
 var ndcDesc ='<h1></h1>';
 
 const standard = [
-    { id: 1, name: 'NDC Conferences', image: '/static/sponsors/NDC_Conferences.png', link: 'https://ndcconferences.com/', description: ndcDesc },
+    { id: 1, name: 'NDC Conferences', image: '/static/sponsors/NDC_Conferences.png', link: 'https://ndcconferences.com/', description: '' },
 ];
 
 export default standard;
--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,6 @@ export default () => (
         </Head>
         <Header title={'DDD East Midlands'}/>
 
-
-        <h2 className="ticket-header">First Wave Of Tickets Go Live: Saturday 23rd March at 9am!</h2>
-        <TicketButton/>
-
         <section>
             <p>
                 DDD East Midlands is an inclusive, not-for-profit technology conference that celebrates the unique tech,
