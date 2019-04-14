a1d3ad1b8cbd5a0cead5d071600e4b18f918abe0
Jessica White <me@jesswhite.co.uk>
2019-04-14 14:36:01 +0100

    Add Microlise logo

--- data/silver.js
+++ data/silver.js
@@ -1,7 +1,9 @@
-const unidaysDesc = '<h1>UNiDAYS.</h1><p>The world\'s leading Student Affinity Network with access to millions of college and university students around the world. We connect brands with Gen Z all in one platform.</p><a href="https://corporate.myunidays.com/en-gb/" target="_blank" category="notable-companies" eventType="UNiDAYS">Learn More</a>'
+const unidaysDesc = '<h1>UNiDAYS.</h1><p>The world\'s leading Student Affinity Network with access to millions of college and university students around the world. We connect brands with Gen Z all in one platform.</p><a href="https://corporate.myunidays.com/en-gb/" target="_blank" category="notable-companies" eventType="UNiDAYS">Learn More</a>';
+const microliseDesc = '<h1>Microlise.</h1><p> A company passionate about fostering an environment where individuals can thrive and we understand that the skill, aptitude and attitude of the people within Microlise are what makes us a success. With generous employee benefits and a structured path to career progression, you could be part of a winning team with boundless opportunities.</p><ul><li><a href="https://www.microlise.com/careers/" target="_blank" category="notable-companies" eventType="Microlise">Careers</a></li></ul>';
 
 const silver = [
     { id: 1, name: 'UNiDAYS', image: '/static/sponsors/UNiDAYS_200.png', link: 'https://www.myunidays.com/GB/en-GB/careers', description: unidaysDesc },
+    { id: 2, name: 'Microlise', image: '/static/sponsors/Microlise_800.png', link: 'https://www.microlise.com/careers/', description: microliseDesc },
 ];
 
 export default silver;
