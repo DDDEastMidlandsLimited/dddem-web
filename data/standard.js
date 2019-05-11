b5f152ada70330a041f30a82a46beeed9950fb6a
Jessica White <me@jesswhite.co.uk>
2019-05-11 19:30:28 +0100

    Add distinct logo and description to NDC

--- data/standard.js
+++ data/standard.js
@@ -1,5 +1,8 @@
+const ndcDesc = '<h1>NDC Conferences</h1><p>NDC Conferences are found across the globe with conferences in Oslo, London, Sydney, Porto, Minnesota and Copenhagen.</p><ul><li><a href="https://ndcconferences.com/" target="_blank" category="notable-companies" eventType="NDC Conferences">Check out our conferences here</a></li></ul>'
+
 const standard =    [
-    { id: 1, name: 'NDC Conferences', image: '/static/sponsors/NDC_Conferences.png', link: 'https://ndcconferences.com/', description: '' },
+    { id: 1, name: 'NDC Conferences', image: '/static/sponsors/NDC_Conferences.png', link: 'https://ndcconferences.com/', description: ndcDesc },
+    { id: 2, name: 'Distinct Recruitment', image: '/static/sponsors/DistinctRec.png', link: 'https://distinctrecruitment.com/get-in-touch/', description: '' }
 ]
 
 export default standard
