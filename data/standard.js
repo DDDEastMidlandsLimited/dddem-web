548dc9c0e481c20164999debc9d04a5f459b2fe0
Jessica White <me@jesswhite.co.uk>
2019-05-22 20:07:31 +0100

    Add Razor Logo and Text

--- data/standard.js
+++ data/standard.js
@@ -1,8 +1,12 @@
 const ndcDesc = '<h1>NDC Conferences</h1><p>NDC Conferences are found across the globe with conferences in Oslo, London, Sydney, Porto, Minnesota and Copenhagen.</p><ul><li><a href="https://ndcconferences.com/" target="_blank" category="notable-companies" eventType="NDC Conferences">Check out our conferences here</a></li></ul>'
+const razorDesc = '<h1>Razor</h1><p>Razor is a business who’s entire purpose for being is to keep its clients, regardless of sector informed and able to exploit the very latest in technology.</p><p>The business is comprised of Microsoft focused software engineers and usability experts, whose job it is, to sit on the ‘cutting edge’ and to keep pace with continuously changing technologies, adopting and adapting new tools and techniques.</p><p>To do this, we regularly push into the bleeding edge (meaning our clients don’t have to) so that we know when a technology is mature enough for adoption. We learn constantly, we read, we explore, we teach and we write. People come to us for advice. Brands come to us to stay competitive.</p><p>We have a reputation for high standards, for doing things that others would shy away from and doing them well. The results we generate, back up our reputation.</p>';
+
+
 
 const standard =    [
     { id: 1, name: 'NDC Conferences', image: '/static/sponsors/NDC_Conferences.png', link: 'https://ndcconferences.com/', description: ndcDesc },
-    { id: 2, name: 'Distinct Recruitment', image: '/static/sponsors/DistinctRec.png', link: 'https://distinctrecruitment.com/get-in-touch/', description: '' }
+    { id: 2, name: 'Razor', image: '/static/sponsors/Razor.png', link: 'https://www.razor.co.uk/', description: razorDesc },
+    { id: 3, name: 'Distinct Recruitment', image: '/static/sponsors/DistinctRec.png', link: 'https://distinctrecruitment.com/get-in-touch/', description: '' },
 ]
 
 export default standard
