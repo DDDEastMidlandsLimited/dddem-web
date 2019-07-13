832496d1b5187a423a807d8aa2be96db43531fff
Jessica White <me@jesswhite.co.uk>
2019-07-13 18:21:42 +0100

    Add keynote description

--- pages/speakers.js
+++ pages/speakers.js
@@ -12,9 +12,21 @@ export default () => (
         </Head>
         <Header title={'Speakers'}/>
         <section>
-            <h1>Keynote By Dylan Beattie</h1>
+            <h1>Keynote By Dylan Beattie
+                <Link target="_blank" href="dylanbeattie" onSelect={event => {
+                    logEvent("twitter", "dylanbeattie")}}>
+                        <a target="_blank"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+                </Link>
+            </h1>
+            <h2>The Art of Code</h2>
             <p>
-                To be confirmed
+                Software and technology has changed every aspect of the world we live in. At one extreme are the ‘mission critical’ applications - the code that runs our banks, our hospitals, our airports and phone networks. Then there’s the code we all use every day to browse the web, watch movies, create spreadsheets… not quite so critical, but still code that solves problems and delivers services. 
+            </p>
+            <p>
+                But what about the code that only exists because somebody wanted to write it? Code created just to make people smile, laugh, maybe even dance? Maybe even code that does nothing at all, created just to see if it was possible?
+            </p>
+            <p>
+                Join Dylan Beattie - programmer, musician, and creator of the Rockstar programming language - for an entertaining look at the art of code. We’ll look at the origins of programming as an art form, from Conway's Game of Life to the 1970s demoscene and the earliest Obfuscated C competitions. We’ll talk about esoteric languages and quines - how DO you create a program that prints its own source code? We’ll look at quine relays, code golf and generative art, and we’ll explore the phenomenon of live coding as performance - from the pioneers of electronic music to modern algoraves and live coding platforms like Sonic Pi. 
             </p>
         </section>
 
