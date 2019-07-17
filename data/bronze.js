a807db47926be9cd13417607d83cec48b8de49ce
Jessica White <me@jesswhite.co.uk>
2019-07-17 19:48:41 +0100

    add cordius logo

--- data/bronze.js
+++ data/bronze.js
@@ -1,8 +1,9 @@
 const frflDesc = '<h1>First Response Finance Limited</h1><p>We’re an award winning finance company based in Nottingham and part of a large international organisation, Itochu Corporation. Our purpose is to provide great credit solutions for our customers despite their financial history. As a business, we truly ensure that what matters to our customers is at the heart of what we do.</p>'
-
+const cordiusDesc =""
 
 const bronze = [
     { id: 1, name: 'First Response Finance', image: '/static/sponsors/FirstResponseFinance_693x242.png', link: 'https://www.firstresponsefinance.co.uk/about/', description: frflDesc },
+    { id: 2, name: 'Cordius', image: '/static/sponsors/CordiusLogo.png', link: 'https://www.cordius.co.uk/jobs', description: cordiusDesc },
 ]
 
 export default bronze
