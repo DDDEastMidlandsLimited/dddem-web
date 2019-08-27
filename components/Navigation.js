adc708b156ba3657c032575a6e7c1fb86dff7fea
Jessica White <me@jesswhite.co.uk>
2019-08-27 17:47:17 +0100

    Delete FAQ page

--- components/Navigation.js
+++ components/Navigation.js
@@ -25,11 +25,8 @@ class Navigation extends Component {
                         <NavDropdown.Item  href="/about">About Us</NavDropdown.Item>
                         <NavDropdown.Item  href="/venue">Venue</NavDropdown.Item>
                         <NavDropdown.Item href="/accessibility">Accessibility</NavDropdown.Item>
-                        <NavDropdown.Item href="/faq">FAQ</NavDropdown.Item>
                         <NavDropdown.Item href="/information/speaker-information">Speaker Information</NavDropdown.Item>
                         <NavDropdown.Item href="/information/sponsor-information">Sponsor Information</NavDropdown.Item>
-                        {/* <NavDropdown.Item href="/attendee-information">Attendee Information</NavDropdown.Item> */}
-                        {/* <NavDropdown.Item href="/contact-us">Contact Us</NavDropdown.Item> */}
                     </NavDropdown>
 
                     <Nav.Link href="/speakers">Speakers</Nav.Link>
