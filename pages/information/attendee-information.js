d12e1fc66ce292155eb52fd1b2fdee247fe5175f
Jessica White <me@jesswhite.co.uk>
2019-09-01 16:30:07 +0100

    Add pubconf information

--- pages/information/attendee-information.js
+++ pages/information/attendee-information.js
@@ -50,8 +50,13 @@ export default () => (
                 <li><a href="#photographs">Photographs</a></li>
                 <li><a href="#social-posts">Social Media Posts</a></li>
             </ul>
-            <p><strong><a href="#pubconf">The Unofficial After Party</a></strong></p>
 
+            <p><strong><a href="#pubconf">PubConf - The Unofficial After Party</a></strong></p>
+            <ul>
+                <li><a href="#after-party-about">About</a></li>
+                <li><a href="#after-party-rules">Rules of PubConf</a></li>
+                <li><a href="#pubconf-tickets">Be alerted about tickets</a></li>
+            </ul>
 
         </section>
         
@@ -306,34 +311,67 @@ export default () => (
             <ul>Instagram: <a href="https://www.instagram.com/dddeastmidlands/?hl=en" target="_blank">@dddeastmidlands</a></ul>
         </section> 
 
+
         <section id="pubconf">
-        <a name="pubconf"/>
-        <h1>The Unofficial After Party</h1>            
-            <h2>PubConf</h2>
+        <h1>PubConf - The Unofficial After Party</h1> 
+
+             <a name="after-party-about"/>
+            <h2>About</h2>
             <p>
-                After the conference there will be an unofficial after party at <a href="https://montysbarandsocial.co.uk" target="_blank">Montys Bar and Social</a>
+                After the conference, there will be a special <a href="https://pubconf.io/events/2019/nottingham/" target="_blank">PubConf</a> featuring some of our speakers and special guests. This 
+                evening event has comedy talks, music, food and beverages. It's great fun for all and we encourage you to come along.
             </p>
             <p>
-                PubConf is an evening of comedy and music. Selected speakers will give humorous talks against the clock. All talks are "Ignite" talks. Speakers have 
-                20 slides which self increment every 15 seconds. The top three presenters then go on to compete for prizes by presenting ignite decks which they have never 
-                seen before.
+                <strong>Quick about:</strong>
+                <ul>
+                    <li>
+                        The talks conducted at PubConf are 5 minute, comedy ignite talks:
+                    </li>
+                    <li>
+                        20 slides timed to increment every 15 seconds. The speakers have no control
+                    </li>
+                    <li>
+                        The top three talks will go into a final battle to win prizes. These three finalists will be presented 
+                        with a ignite deck they have never seen before and given a topic. They will then have to come up with an 
+                        impromptu talk on the spot.
+                    </li>
+                    <li>
+                        Talk topics have an emphasis on humour. Often the content is not suitable to be shared away 
+                        from the safety of PubConf.
+                    </li>
+                    <li>
+                        PubConf is a separate entity and business from DDD East Midlands. As such they have their own 
+                        Code of Conduct. Hence "Unofficial" after party. We are not involved in its organisation.
+                    </li>
+                </ul>
             </p>
             <p>
-                Remember with these talks, what happens at PubConf, stays at PubConf. The talks can not be photographed, filmed or quoted without the speakers express permission.
+                This is the first PubConf in the UK outside of London and it's going to be brilliant.
+            </p>
+
+             <FullWidthImage url={'/static/banners/pubconf.png'}/>
+
+             <a name="after-party-rules"/>
+            <h2>Rules of PubConf</h2>
+            <p>
+                <strong>What happens at PubConf, stays at Pubconf.</strong>
+                <ul>
+                    <li>No photos or videos unless you have the speakers explicit permission.</li>
+                    <li>No quoting talks. (Taken out of context, a joke can be damaging)</li>
+                </ul>
+                <strong>This is an event for over 18's as there will be alcohol present.</strong>
+            </p>
+
+             <a name="pubconf-tickets"/>
+            <h2>Be alerted about tickets</h2>
+            <p>
+                Go to the <a href="https://pubconf.io/events/2019/nottingham/" target="_blank">PubConf website</a> to find out more and 
+                sign up for an email alert when tickets are released.
             </p>
             <p>
-                There will also be music from Dylan Beattie, drinks and food.
+                Anyone can buy a ticket to PubConf, even if they aren't attending the DDD East Midlands conference.
             </p>
-            <p>
-                This is an event for over 18's as there will be alcohol present. It will be a great evening and we highly recommend you come along. 
-            </p>
-            <p>
-                Tickets are to be released soon and ANYONE can buy a ticket. You don't have to come to DDD East Midlands Conference to attend PubConf.
-            </p>
-            <p>
-                Find out more about PubConf <a href="https://pubconf.io/" target="_blank">here</a>.
-            </p>
-        </section>
+         </section>
 
         <style jsx>
             {`
