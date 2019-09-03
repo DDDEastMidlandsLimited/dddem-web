65f0015b637c6a321013f56903e59ee6c02d421c
Jessica White <me@jesswhite.co.uk>
2019-09-03 20:02:44 +0100

    Move Agenda Page

--- components/Navigation.js
+++ components/Navigation.js
@@ -26,10 +26,10 @@ class Navigation extends Component {
                         <NavDropdown.Item  href="/venue">Venue</NavDropdown.Item>
                         <NavDropdown.Item href="/accessibility">Accessibility</NavDropdown.Item>
                         <NavDropdown.Item href="/faq">FAQ</NavDropdown.Item>
-                        <NavDropdown.Item href="/agenda">Agenda</NavDropdown.Item>
                         <NavDropdown.Item href="/information/speaker-information">Speaker Information</NavDropdown.Item>
                         <NavDropdown.Item href="/information/sponsor-information">Sponsor Information</NavDropdown.Item>
                     </NavDropdown>
+                    <Nav.Link href="/agenda">Agenda</Nav.Link>
 
                     <Nav.Link href="/speakers">Speakers</Nav.Link>
 
