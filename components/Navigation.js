1edfbc50684af0d90046dd412a0e00b36d629619
Jessica White <me@jesswhite.co.uk>
2019-09-08 14:34:24 +0100

    Move Application Information to it's own page and delete FAQ

--- components/Navigation.js
+++ components/Navigation.js
@@ -24,8 +24,8 @@ class Navigation extends Component {
                     <NavDropdown title="About" id="basic-nav-dropdown">
                         <NavDropdown.Item  href="/about">About DDDEM</NavDropdown.Item>
                         <NavDropdown.Item  href="/venue">Venue</NavDropdown.Item>
-                        <NavDropdown.Item href="/information/accessibility">Accessibility</NavDropdown.Item>
-                        <NavDropdown.Item href="/faq">FAQ</NavDropdown.Item>
+                        <NavDropdown.Item href="/information/accessibility">Accessibility Information</NavDropdown.Item>
+                        <NavDropdown.Item href="/information/application-information">Applying To Speak</NavDropdown.Item>
                         <NavDropdown.Item href="/information/attendee-information">Attendee Information</NavDropdown.Item>
                         <NavDropdown.Item href="/information/speaker-information">Speaker Information</NavDropdown.Item>
                         <NavDropdown.Item href="/information/sponsor-information">Sponsor Information</NavDropdown.Item>
---
+++ pages/information/application-information.js
@@ -0,0 +1,69 @@
+import Layout from '../../components/Layout'
+import Header from "../../components/Header"
+import theme from "../../theme/theme"
+import Head from "next/head"
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>Application Information</title>
+        </Head>
+        <Header title={'Application Information'}/>
+
+        <section name="Contents">
+            <h1>Contents</h1>
+
+            <p>Applying to Speak</p>
+            <ul>
+                <li><a href="#applicationdate">When can I apply to talk?</a></li>
+                <li><a href="#applicationhow">How do I apply to talk?</a></li>
+                <li><a href="#tande">Are travel and accomodation expenses provided for speakers?</a></li>
+            </ul>
+        </section>
+
+        <section id="title">
+                    <h1>Applying to Speak</h1>
+        </section>
+
+       
+        <section id="date">
+            <a name="applicationdate"/>
+            <h2>When can I apply to talk?</h2>
+            <p>
+                Applications to speak at the 2019 event have closed. 2020 applications open early next year.
+            </p>
+        </section>
+
+        <section id="how">
+            <a name="applicationhow"/>
+            <h2>How do I apply to talk?</h2>
+            <p>
+                When applications are open <a href="https://sessionize.com/dddeastmidlands/" target="_blank"> you can submit your talk through Sessionize.</a>
+            </p>
+            <p>
+                <a href="https://medium.com/dddeastmidlands/using-sessionize-to-submit-f16f702c4087" target="_blank">See here for how to use Sessionize to apply for a talk.</a>
+            </p>
+        </section>
+
+        <section id="tande">
+            <a name="tande"/>
+            <h2>Are travel and accomodation expenses provided for speakers?</h2>
+            <p>
+                As we want to encourage local speakers and keep our costs low, we cannot provide expenses (i.e. travel and accomodation costs) to speakers.
+            </p> 
+        </section>
+       
+        <style jsx>
+            {`
+            section {
+                max-width: ${theme.sizes.maxContentWidth};
+                padding: ${theme.sizes.contentPadding};
+                margin: auto;
+            }
+            section div{
+                padding-bottom: 10px;
+            }
+        `}
+        </style>
+    </Layout>
+)
