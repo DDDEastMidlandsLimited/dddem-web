238941d28afca9a61c866c3faf86ad5e95dce1be
Jessica White <me@jesswhite.co.uk>
2019-10-20 18:28:26 +0100

    Add Searchability to Notable Companies

--- data/standard.js
+++ data/standard.js
@@ -4,6 +4,8 @@ const razorDesc =
   '<h1>Razor</h1><p>Razor is a business who’s entire purpose for being is to keep its clients, regardless of sector informed and able to exploit the very latest in technology.</p><p>The business is comprised of Microsoft focused software engineers and usability experts, whose job it is, to sit on the ‘cutting edge’ and to keep pace with continuously changing technologies, adopting and adapting new tools and techniques.</p><p>To do this, we regularly push into the bleeding edge (meaning our clients don’t have to) so that we know when a technology is mature enough for adoption. We learn constantly, we read, we explore, we teach and we write. People come to us for advice. Brands come to us to stay competitive.</p><p>We have a reputation for high standards, for doing things that others would shy away from and doing them well. The results we generate, back up our reputation.</p>';
 const minderaDesc =
   '<h1>Mindera</h1><p>At Mindera we use technology to build products we are proud of, with people we love.  We partner with our clients, to understand their product and deliver high-performance, resilient and scalable software systems that create an impact on their users and businesses across the world.</p><p>We work with a bunch of great people, where the whole team owns the project together. Our culture reflects our lean and self-organisation attitude. We encourage our colleagues to take risks, make decisions, work in a collaborative way and talk to everyone to enhance communication.  We are proud of our work and we love to learn!</p><ul><li><a href="https://www.facebook.com/minderauksoftwarecraft/" target="_blank" category="notable-companies" eventType="Mindera">Facebook</a></li><li><a href="https://www.linkedin.com/company/mindera-uk/" target="_blank" category="notable-companies" eventType="Mindera">LinkedIn</a></li><li><a href="https://twitter.com/minderauk/" target="_blank" category="notable-companies" eventType="Mindera">Twitter</a></li><li><a href="https://www.instagram.com/mindera_uk/" target="_blank" category="notable-companies" eventType="Mindera">Instagram</a></li><li><a href="https://mindera.com/" target="_blank" category="notable-companies" eventType="Mindera">Website</a></li></ul>';
+const searchabilityDesc =
+  '<h1>Searchability</h1><p>Searchability are one of the UK’s fastest growing IT and digital agencies, specialising in sourcing the best tech talent across both permanent and contract markets. With multiple awards under our belt, we have successfully combined proven recruitment solutions with employer branding, inbound recruitment marketing and bespoke engagement technology to create a very unique full-service recruitment agency for our clients.</p>';
 
 const standard = [
   {
@@ -29,6 +31,13 @@ const standard = [
   },
   {
     id: 4,
+    name: 'Searchability',
+    image: '/static/sponsors/Searchability.png',
+    link: 'https://searchability.co.uk/',
+    description: searchabilityDesc,
+  },
+  {
+    id: 5,
     name: 'Distinct Recruitment',
     image: '/static/sponsors/DistinctRec.png',
     link: 'https://distinctrecruitment.com/get-in-touch/',
