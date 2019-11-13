958731044afe0cf8049a5eba1ff73f93b3f168c3
Jessica White <me@jesswhite.co.uk>
2019-11-13 20:07:16 +0000

    Put buttons into two columns

--- pages/about/about.js
+++ pages/about/about.js
@@ -11,86 +11,140 @@ export default () => (
     </Head>
     <Header title={'Information Pages'} />
 
-    <section id="about-the-conference">
-      <div>
-        <a
-          tabIndex="0"
-          href="/about/about-the-conference"
-          title="Link to Speaker Information Page"
-        >
-          <img src="/static/banners/home.jpg" alt="Alt text" />
-          <div className="text">Conference Information</div>
-        </a>
-      </div>
-    </section>
+    <article>
+      <section id="about-the-conference">
+        <div>
+          <a
+            tabIndex="0"
+            href="/about/about-the-conference"
+            title="Link to Speaker Information Page"
+          >
+            <div className="conference-button">
+              <h2>Conference Information</h2>
+            </div>
+          </a>
+        </div>
+      </section>
 
-    <section id="venue">
-      <div>
-        <a
-          tabIndex="0"
-          href="/about/venue"
-          title="Link to Speaker Information Page"
-        >
-          <img src="/static/banners/thencc.jpg" alt="Alt text" />
-          <div className="text">Venue Information</div>
-        </a>
-      </div>
-    </section>
+      <section id="venue">
+        <div>
+          <a
+            tabIndex="0"
+            href="/about/venue"
+            title="Link to Speaker Information Page"
+          >
+            <div className="venue-button">
+              <h2>Venue Information</h2>
+            </div>
+          </a>
+        </div>
+      </section>
 
-    <section id="tickets">
-      <div>
-        <a
-          tabIndex="0"
-          href="/about/ticket-information"
-          title="Link to Ticket Information Page"
-        >
-          <img
-            src="/static/information/speakers.jpg"
-            alt="Alt text"
-          />
-          <div className="text">Ticket Information</div>
-        </a>
-      </div>
-    </section>
+      <section id="tickets">
+        <div>
+          <a
+            tabIndex="0"
+            href="/about/ticket-information"
+            title="Link to Ticket Information Page"
+          >
+            <div className="ticket-button">
+              <h2>Ticket Information</h2>
+            </div>
+          </a>
+        </div>
+      </section>
 
-    <section id="speaker-information">
-      <div>
-        <a
-          tabIndex="0"
-          href="/about/speaker-information"
-          title="Link to Speaker Information Page"
-        >
-          <img
-            src="/static/information/speakers.jpg"
-            alt="Alt text"
-          />
-          <div className="text">Speaker Information</div>
-        </a>
-      </div>
-    </section>
+      <section id="speaker-information">
+        <div>
+          <a
+            tabIndex="0"
+            href="/about/speaker-information"
+            title="Link to Speaker Information Page"
+          >
+            <div className="speaker-button">
+              <h2>Speaker Information</h2>
+            </div>
+          </a>
+        </div>
+      </section>
+    </article>
 
     <style jsx>
       {`
-        img {
+        .conference-button {
+          background-image: url('/static/banners/home.jpg');
+          background-size: cover;
+          width: 100%;
           height: 200px;
-          width 100%;
-          object-fit: cover;
-          object-position: center;
-          overflow: hidden;
+          background-position: center;
         }
 
-        .text {
+        .conference-button h2 {
           color: white;
-          margin-top: -100px;
           background: ${theme.palette.dark};
           font-size: 24px;
-          position: absolute;
-          padding: 5px 5px;
-          left: 50%;
-          -webkit-transform: translate(-50%, -50%);
-          -ms-transform: translate(-50%, -50%);
-          transform: translate(-50%, -50%);
           text-align: center;
+          position: absolute;
+          padding: 10px 10px;
+        }
+
+        .venue-button {
+          background-image: url('/static/banners/thencc.jpg');
+          background-size: cover;
+          width: 100%;
+          height: 200px;
+          background-position: center;
+        }
+
+        .venue-button h2 {
+          color: white;
+          background: ${theme.palette.dark};
+          font-size: 24px;
+          text-align: center;
+          position: absolute;
+          padding: 10px 10px;
+        }
+
+        .ticket-button {
+          background-image: url('/static/information/speakers.jpg');
+          background-size: cover;
+          width: 100%;
+          height: 200px;
+          background-position: center;
+        }
+
+        .ticket-button h2 {
+          color: white;
+          background: ${theme.palette.dark};
+          font-size: 24px;
+          text-align: center;
+          position: absolute;
+          padding: 10px 10px;
+        }
+
+        .speaker-button {
+          background-image: url('/static/information/speakers.jpg');
+          background-size: cover;
+          width: 100%;
+          height: 200px;
+          background-position: center;
+        }
+
+        .speaker-button h2 {
+          color: white;
+          background: ${theme.palette.dark};
+          font-size: 24px;
+          text-align: center;
+          position: absolute;
+          padding: 10px 10px;
+        }
+
+        article {
+          -webkit-columns: 2 400px;
+          -moz-columns: 2 400px;
+          columns: 2 400px;
+          margin-left: 10%;
+          margin-right: 10%;
         }
 
         section {
@@ -98,7 +152,10 @@ export default () => (
           padding: ${theme.sizes.contentPadding};
           margin: auto;
         }
-       
+
+        section:hover {
+          opacity: 0.4;
+        }
       `}
     </style>
   </Layout>
