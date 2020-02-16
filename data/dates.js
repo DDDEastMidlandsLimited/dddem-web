3a98ac94d009dc5a53e5dfdc05dce1f97888879f
Jessica White <me@jesswhite.co.uk>
2020-02-16 16:51:26 +0000

    Format date file

--- data/dates.js
+++ data/dates.js
@@ -1,6 +1,14 @@
 const dates = [
-  { id: 1, date: '7th Feburary', name: 'Call For Speakers Opens' },
-  { id: 2, date: '16th March', name: 'Call For Speakers Closes' },
+  {
+    id: 1,
+    date: '7th Feburary',
+    name: 'Call For Speakers Opens',
+  },
+  {
+    id: 2,
+    date: '16th March',
+    name: 'Call For Speakers Closes',
+  },
   {
     id: 3,
     date: '20th March',
@@ -11,9 +19,21 @@ const dates = [
     date: '10th April',
     name: 'Second Wave Of Tickets Released',
   },
-  { id: 5, date: '13th April', name: 'Voting Opens' },
-  { id: 6, date: '11th May', name: 'Voting Closes' },
-  { id: 7, date: '3rd September', name: 'Agenda Published' },
+  {
+    id: 5,
+    date: '13th April',
+    name: 'Voting Opens',
+  },
+  {
+    id: 6,
+    date: '11th May',
+    name: 'Voting Closes',
+  },
+  {
+    id: 7,
+    date: '3rd September',
+    name: 'Agenda Published',
+  },
   {
     id: 8,
     date: '3rd October',
