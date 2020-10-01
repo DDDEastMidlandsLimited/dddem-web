588f780c23dec49033c61d146454751e8d9ea135
John Nolan <me@johnnolan.dev>
2020-10-01 20:07:39 +0100

    Added sitemap.xml on generation of site (#174)
    * Added sitemap.xml on generation of site
    
    * Minor end of line fix

---
+++ utils/generateSiteMap.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const globby = require('globby');
+
+async function generateSiteMap() {
+  const pages = await globby([
+    'pages/**/*.js',
+    '!pages/_*.js',
+    '!pages/**/[id].js',
+    '!pages/api',
+  ]);
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace('pages', '')
+              .replace('.js', '')
+              .replace('.md', '');
+            const route = path === '/index' ? '' : path;
+            return `
+        <url>
+            <loc>${`https://www.dddeastmidlands.com${route}`}</loc>
+        </url>
+    `;
+          })
+          .join('')}
+    </urlset>
+`;
+
+  fs.writeFileSync('out/sitemap.xml', sitemap);
+}
+
+generateSiteMap();
