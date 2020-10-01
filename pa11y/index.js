7248e1e740daee2a5fd260ab33d3cb3e3aea3bf5
John Nolan <john.nolan@digital.justice.gov.uk>
2020-10-01 17:52:42 +0100

    Moved tests to await all and fail if they don't pass

--- pa11y/index.js
+++ pa11y/index.js
@@ -1,14 +1,28 @@
 const globby = require('globby');
 const pa11y = require('pa11y');
 
+const runPa11y = async (page) => {
+  return pa11y(`./${page}`);
+};
+
 async function pa11yCheck() {
   const pages = await globby(['out/**/*.html']);
 
-  pages.map((page) => {
-    pa11y(`./${page}`).then((results) => {
-      console.log(results);
-    });
+  const full_results = await Promise.all(
+    pages.map((page) => {
+      return runPa11y(`./${page}`);
+    }),
+  );
+
+  full_results.map(function (result) {
+    console.log(result);
   });
+
+  if (full_results.length > 0) {
+    process.exit(2);
+  } else {
+    process.exit(0);
+  }
 }
 
 pa11yCheck();
