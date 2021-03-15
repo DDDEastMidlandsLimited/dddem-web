3f0c57f4c2f88e6e506ad5cb9a258d30263e44bb
Jessica <me@jesswhite.co.uk>
2021-03-15 15:33:17 +0000

    Add Tickets link to Nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -49,17 +49,17 @@ class Navigation extends Component {
                 <Nav.Link href="/2020/sponsor">Sponsor</Nav.Link>
               </Nav.Item>
 
-              {/*               <Nav.Item>
+              <Nav.Item>
                 <Nav.Link
                   target="_blank"
-                  href="https://www.eventbrite.co.uk/e/ddd-east-midlands-2020-tickets-95834801471"
-                  onSelect={event => {
+                  href="https://www.eventbrite.co.uk/e/ddd-east-midlands-2021-tickets-146129646707"
+                  onSelect={(event) => {
                     logEvent('navigation', 'eventbrite');
                   }}
                 >
                   Tickets
                 </Nav.Link>
-              </Nav.Item> */}
+              </Nav.Item>
 
               <Nav.Item>
                 <Nav.Link
