bfbe070c4a4734ecdedb23d6fa0100e5589d76dc
Jessica <me@jesswhite.co.uk>
2021-03-16 20:25:43 +0000

    Fix size and styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Link from 'next/link';
 import theme from '../theme/theme';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 /* enabled prettier*/
 
 const Footer = () => (
   <footer>
-    <ul>
-      <li>
+    <Row>
+      <Col xs="3" m="4">
         <Link href="/code-of-conduct">
           <a target="_blank">Code of Conduct</a>
         </Link>
-      </li>
-      <li>
+      </Col>
+      <Col xs="3" m="4">
         <Link
           href="https://twitter.com/dddeastmidlands"
           prefetch={false}
@@ -22,8 +24,6 @@ const Footer = () => (
             <FontAwesomeIcon icon={['fab', 'twitter']} size="sm" />
           </a>
         </Link>
-      </li>
-      <li>
         <Link
           href="https://github.com/DDDEastMidlandsLimited/dddem-web"
           prefetch={false}
@@ -32,8 +32,6 @@ const Footer = () => (
             <FontAwesomeIcon icon={['fab', 'github']} size="sm" />
           </a>
         </Link>
-      </li>
-      <li>
         <Link
           href="https://www.instagram.com/dddeastmidlands/?hl=en"
           prefetch={false}
@@ -42,8 +40,6 @@ const Footer = () => (
             <FontAwesomeIcon icon={['fab', 'instagram']} size="sm" />
           </a>
         </Link>
-      </li>
-      <li>
         <Link
           href="https://www.linkedin.com/company/ddd-east-midlands-limited/"
           prefetch={false}
@@ -52,13 +48,13 @@ const Footer = () => (
             <FontAwesomeIcon icon={['fab', 'linkedin']} size="sm" />
           </a>
         </Link>
-      </li>
-      <li>
+      </Col>
+      <Col xs="3" m="3">
         <Link href="/contact">
           <a>Contact</a>
         </Link>
-      </li>
-    </ul>
+      </Col>
+    </Row>
     <style jsx>
       {`
         footer {
@@ -66,38 +62,13 @@ const Footer = () => (
           background-color: ${theme.palette.primary};
           padding: 20px 0;
           margin-top: 30px;
-        }
-        ul {
-          display: flex;
-          margin: 0;
-          padding: 10px;
-          flex-flow: column;
-          justify-content: space-evenly;
-          flex-wrap: nowrap;
-        }
-        li {
-          margin: 0;
-          padding: 10px;
-          list-style: none;
-          list-style-type: none;
           text-align: center;
-          flex-basis: 0;
-          flex-grow: 1;
         }
         a {
           text-decoration: none;
           font-family: ${theme.font.default};
           color: ${theme.palette.light};
-        }
-        @media (max-width: 567px) {
-          ul {
-            display: flex;
-            margin: 0;
-            padding: 10px;
-            flex-flow: column;
-            justify-content: space-evenly;
-            flex-wrap: nowrap;
-          }
+          margin-left: 5%;
         }
       `}
     </style>
