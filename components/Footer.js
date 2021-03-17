92501715fdbdc8a9359a263ab83270b2d0d30691
Jessica <me@jesswhite.co.uk>
2021-03-17 19:43:25 +0000

    Remove unused code from Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import Link from 'next/link';
 import theme from '../theme/theme';
 import '@fortawesome/fontawesome-svg-core/styles.css';
