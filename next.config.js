aa0f7bd4b8d7b1eec49e24bd9ba41f11fe6c8cc5
John Nolan <me@johnnolan.dev>
2021-04-14 20:44:02 +0100

    Fix for misconfigured next.config.js

--- next.config.js
+++ next.config.js
@@ -10,34 +10,32 @@ module.exports = withPlugins(
       optimizeImagesInDev: true,
     },
   ],
-  [
-    withPWA({
-      pwa: {
-        dest: 'public',
-      },
-      trailingSlash: true,
-      exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId },
-      ) {
-        const pathMap = {};
-        glob.sync('pages/**/*.js').forEach((s) => {
-          if (s.indexOf('_') === -1) {
-            const path = s
-              .split(/(pages|\.)/)[2]
-              .replace(/^\/index$/, '/');
-            pathMap[path] = { page: path };
-          }
-        });
-        return pathMap;
-      },
-      webpack: (config, { isServer }) => {
-        if (isServer) {
-          require('./utils/generateSiteMap');
+  withPWA({
+    pwa: {
+      dest: 'public',
+    },
+    trailingSlash: true,
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId },
+    ) {
+      const pathMap = {};
+      glob.sync('pages/**/*.js').forEach((s) => {
+        if (s.indexOf('_') === -1) {
+          const path = s
+            .split(/(pages|\.)/)[2]
+            .replace(/^\/index$/, '/');
+          pathMap[path] = { page: path };
         }
+      });
+      return pathMap;
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./utils/generateSiteMap');
+      }
 
-        return config;
-      },
-    }),
-  ],
+      return config;
+    },
+  }),
 );
