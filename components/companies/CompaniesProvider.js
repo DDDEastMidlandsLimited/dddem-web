72f419431eda9d1add31322b3b0385aa3c84cc0e
Jessica <me@jesswhite.co.uk>
2021-05-15 16:51:38 +0100

    Fix Notable Companies Grid

--- components/companies/CompaniesProvider.js
+++ components/companies/CompaniesProvider.js
@@ -3,9 +3,11 @@ import CompaniesContext from './CompaniesContext';
 import gold from '../../data/sponsors/gold';
 import silver from '../../data/sponsors/silver';
 import partners from '../../data/sponsors/partners';
+import platinum from '../../data/sponsors/platinum';
 
 export default function CompaniesProvider(props) {
   const [companiesList] = useState([
+    ...platinum,
     ...gold,
     ...silver,
     ...partners,
