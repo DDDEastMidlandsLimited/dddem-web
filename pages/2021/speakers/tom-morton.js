7caee2a028db28a14447179b06ab44072e0ffe2b
Jessica <me@jesswhite.co.uk>
2021-06-19 18:55:42 +0100

    Add Tom's page

---
+++ pages/2021/speakers/tom-morton.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+import Header from '../../../components/Header';
+import theme from '../../../theme/theme';
+import Head from 'next/head';
+import SpeakersButton from '../../../components/speakers/SpeakersButton';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faInstagram,
+  faTwitter,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faInstagram, faTwitter, faGithub, faLinkedin);
+
+export default function TomMorton() {
+  return (
+    <Layout>
+      <Head>
+        <title>Tom Morton</title>
+      </Head>
+      <Header title={'Tom Morton'} />
+      <section>
+        <SpeakersButton />
+        <img
+          src="/static/speakers/2021/Matt_Whetton.png"
+          alt=""
+          className="speakerpic"
+        />
+        <div>
+          <ul>
+            <b>Pronouns: </b> He/Him
+          </ul>
+          <ul>
+            <b>Talk Length: </b> 30 mins
+          </ul>
+        </div>
+        <br />
+        <h2 tabIndex="0">
+          Open Banking vs. DeFi; The technological future of finance?
+        </h2>
+        <p tabIndex="0">
+          Crypto Currencies. Open API's. Democratising FInance. Which
+          approach to Open Finance will ultimately succeed and what
+          does that mean for technology in the future.
+        </p>
+      </section>
+      <section>
+        <h2 tabIndex="0">About Tom</h2>
+        <p tabIndex="0">
+          <b>Senior Engineering Manager, Capital One</b>
+        </p>
+        <p tabIndex="0">
+          Tom is an engineer, writer and experienced people leader.
+          His technical background and interests are extensive
+          including security, software development, DevOps and
+          engineering culture. In 2008 he famously declared management
+          was not for him. Naturally, 6 years later he is responsible
+          for 50 people across a number of teams - although he does
+          try to write code when he can.
+        </p>
+      </section>
+
+      <section>
+        <h2 tabIndex="0">Links</h2>
+        <li>
+          <a
+            target="_blank"
+            aria-label="Tom Morton: Twitter"
+            href="https://twitter.com/errantx"
+          >
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            aria-label="Tom Morton: Website"
+            href="https://errant.me.uk/"
+          >
+            Website
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            aria-label="Tom Morton: LinkedIn"
+            href="https://www.linkedin.com/in/errantx/"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </section>
+
+      <style jsx>
+        {`
+          section {
+            max-width: ${theme.sizes.maxContentWidth};
+            padding: ${theme.sizes.contentPadding};
+            margin: auto;
+          }
+
+          .row:after {
+            content: '';
+            display: table;
+            clear: both;
+          }
+
+          .speakerpic {
+            margin-top: 8%;
+            margin-bottom: 5%;
+            width: 250px;
+            border-radius: 50%;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+
+          .iframe-container {
+            position: relative;
+            overflow: hidden;
+            padding-top: 56.25%;
+            margin: 10px 10px;
+          }
+          .iframe-container iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+          }
+          ul {
+            display: flex;
+            margin: 0;
+            padding: 5px;
+            flex-flow: row;
+            flex-wrap: nowrap;
+            justify-content: center;
+          }
+          li {
+            margin: 0;
+            padding: 10px;
+            list-style: inside;
+            list-style-type: disc;
+            text-align: left;
+            flex-basis: 0;
+            flex-grow: 1;
+          }
+
+          .contactButton {
+            align: ${theme.palette.quaternary};
+            font-weight: bold;
+            font-size: ${theme.font.sizes.mobile.subtitle1};
+            flex-grow: 1;
+            border-radius: 12px;
+            padding: 10px 10px 10px 10px;
+            color: ${theme.palette.primary};
+            border: 2px solid ${theme.palette.primary};
+            background-color: ${theme.palette.light};
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2),
+              0 6px 20px 0 rgba(0, 0, 0, 0.19);
+          }
+          .contactButton a {
+            text-decoration: none;
+          }
+
+          .contactButton:hover {
+            background-color: ${theme.palette.primary};
+          }
+
+          .contactButton a:hover {
+            color: ${theme.palette.light};
+          }
+
+          @media only screen and (max-width: 1081px) {
+            section {
+              padding: 3% 12%;
+            }
+          }
+        `}
+      </style>
+    </Layout>
+  );
+}
