b237cd1e8668a3da3961a43a4cf66d90fa7ad6a6
Jessica <me@jesswhite.co.uk>
2021-06-29 19:41:50 +0100

    Continue merge

--- data/sponsors/silver.js
+++ data/sponsors/silver.js
@@ -1,14 +1,16 @@
 const oakbrookDesc =
   '<h1>Oakbrook: </h1><p>simplifying and personalising borrowing.</p>';
+const koodooDesc =
+  '<h1>Koodoo.</h1><p>The digital platform powering the mortgage industry.</p><p>Koodoo works with lenders and aggregators to intelligently connect homeowners with their next mortgage.</p>'
 
 const silver = [
-  /*   {
+  {
     id: 1,
-    name: '',
-    image: '/static/sponsors/empty.png',
-    link: '',
-    description: "",
-  }, */
+    name: 'Koodoo',
+    image: '/static/sponsors/Koodoo.png',
+    link: 'https://koodoo.io/careers',
+    description: koodooDesc,
+  },
   {
     id: 2,
     name: 'Oakbrook',
--- pages/index.js
+++ pages/index.js
@@ -183,6 +183,10 @@ export default function Index() {
           .infolink {
             margin: 10px;
           }
+          .silver{
+            text-align: center;
+            width: 70%;
+          }
         `}
       </style>
     </Layout>
