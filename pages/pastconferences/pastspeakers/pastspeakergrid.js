b2579753b1105102acfb2d7de7697cfbb253f90d
Jessica <me@jesswhite.co.uk>
2021-07-02 18:21:06 +0100

    Add 2019 speaker photos

--- pages/pastconferences/pastspeakers/pastspeakergrid.js
+++ pages/pastconferences/pastspeakers/pastspeakergrid.js
@@ -32,11 +32,11 @@ export default function Speaker2021Component() {
             alignItems="center"
           >
             <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Dylan_Beattie.png"
-                  alt="Picture of Dylan Beattie"
-                />
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Dylan_Beattie.png"
+                alt="Picture of Dylan Beattie"
+              />
             </Grid>
           </Grid>
         </div>
@@ -51,59 +51,108 @@ export default function Speaker2021Component() {
             alignItems="center"
           >
             <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Matt_Brunt.png"
-                  alt="Picture of Matt Brunt"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Jessica_Salisbury.png"
-                  alt="Picture of Jessica Salisbury"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Anthony_Dang.png"
-                  alt="Picture of Anthony Dang"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Cara_Holland.png"
-                  alt="Picture of Cara Holland"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Mark_Towndrow.png"
-                  alt="Picture of Mark Towndrow"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Galiya_Warrier.png"
-                  alt="Picture of Galiya Warrier"
-                />
-            </Grid>
-            <Grid item xs>
-                <img
-                  className="speaker-image"
-                  src="/static/speakers/2019/Robin_Ninan.png"
-                  alt="Picture of Robin Ninan"
-                />
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Matt_Brunt.png"
+                alt="Picture of Matt Brunt"
+              />
             </Grid>
             <Grid item xs>
               <img
                 className="speaker-image"
-                src="/static/speakers/TBC.png"
-                alt="To be confirmed placeholder"
+                src="/static/speakers/2019/Jessica_Salisbury.png"
+                alt="Picture of Jessica Salisbury"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Anthony_Dang.png"
+                alt="Picture of Anthony Dang"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Cara_Holland.png"
+                alt="Picture of Cara Holland"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Mark_Towndrow.png"
+                alt="Picture of Mark Towndrow"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Galiya_Warrier.png"
+                alt="Picture of Galiya Warrier"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Robin_Ninan.png"
+                alt="Picture of Robin Ninan"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Ian_Johnson.png"
+                alt="Picture of Ian Johnson"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Samathy_Barratt.png"
+                alt="Picture of Samathy Barratt"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Ian_Cooper.png"
+                alt="Picture of Ian Cooper"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Helen_Joy.png"
+                alt="Picture of Helen Joy"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Joel_Hammond-Turner.png"
+                alt="Picture of Joel Hammond-Turner"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Zac_Braddy.png"
+                alt="Picture of Zac Braddy"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Neil_OConnor.png"
+                alt="Picture of Neil O'Connor"
+              />
+            </Grid>
+            <Grid item xs>
+              <img
+                className="speaker-image"
+                src="/static/speakers/2019/Simon_Painter.png"
+                alt="Picture of Simon Painter"
               />
             </Grid>
           </Grid>
@@ -118,7 +167,7 @@ export default function Speaker2021Component() {
             margin: auto;
           }
 
-          .grid-container{
+          .grid-container {
             text-align: center;
           }
 
