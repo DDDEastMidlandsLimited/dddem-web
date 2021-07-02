89bddcf746669e2396882ff46a115e42a552cd9a
Jessica White <me@jesswhite.co.uk>
2021-07-02 08:39:58 +0100

    Enhancement: Add Calyx as Platinum Sponsor (#347)

--- components/companies/CompaniesProvider.js
+++ components/companies/CompaniesProvider.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import CompaniesContext from './CompaniesContext';
+import platinum from '../../data/sponsors/platinum';
 import gold from '../../data/sponsors/gold';
 import silver from '../../data/sponsors/silver';
 
 export default function CompaniesProvider(props) {
-  const [companiesList] = useState([...gold, ...silver]);
+  const [companiesList] = useState([...platinum, ...gold, ...silver]);
   const [selectedCompanyDescription, setSelectedCompanyDescription] =
     useState('');
 
