939a81224209a3fe4cae39f828a1ee77e3ed5e4f
Andy Stannard <andy.stannard@edays.com>
2021-09-29 18:13:22 +0100

    speakers in their own component

--- pages/2021/speakers/speakergrid.js
+++ pages/2021/speakers/speakergrid.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Layout from '../../../components/Layout';
 import theme from '../../../theme/theme';
 import Header from '../../../components/Header';
+import SpeakerGridItemComponent from './speakergriditem';
 import Head from 'next/head';
 
 export default function Speaker2021Component() {
+
+  function popIt(e){
+    console.log(e);
+    var article = e.target;
+    var popper =  ReactDOM.findDOMNode(article).getElementsByClassName('hover-pop');
+    popper.className = popper.className + ' popped';
+  }
+
+  function stopIt(e){
+    console.log(e);
+    var article = e.target;
+    var popper =  ReactDOM.findDOMNode(article).getElementsByClassName('hover-pop');
+   // popper.className = 'hover-pop';
+  }
+
   return (
     <Layout>
       <Head>
@@ -21,243 +38,33 @@ export default function Speaker2021Component() {
         </p>
       </section>
       <section className="speaker-grid">
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../tania-allard"
-            title="Link to Information About Tania"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Tania_Allard.jpg"
-              alt="Picture of Tania Allard"
-            />
-          </a>
-        </article>
+          <SpeakerGridItemComponent speakername="Tania" pic="/static/speakers/2021/Tania_Allard.jpg" link="../tania-allard" talkbrief=" " />
       </section>
-      <section className="speaker-grid">
-        <article className="speaker-grid-item">
-          <a
-            className="speaker-link"
-            tabIndex="0"
-            href="../lex-lofthouse"
-            title="Link to Information About Lex"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Lex_Lofthouse.png"
-              alt="Picture of Lex Lofthouse"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../connell-w"
-            title="Link to Information About Connell"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Connell_W.png"
-              alt="Picture of Connell"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../lucy-fletcher"
-            title="Link to Information About Lucy"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Lucy_Fletcher.png"
-              alt="Picture of Lucy Fletcher"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../simon-painter"
-            title="Link to Information About Simon"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Simon_Painter.png"
-              alt="Picture of Simon Painter"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../steve-collins"
-            title="Link to Information About Steve"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Steve_Collins.jpg"
-              alt="Picture of Steve Collins"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../stephen-haunts"
-            title="Link to Information About Stephen"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Stephen_Haunts.jpg"
-              alt="Picture of Stephen Haunts"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../manu-magalhaes"
-            title="Link to Information About Manu"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Manu_Magalhaes.png"
-              alt="Picture of Manu Magalhaes"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../james-croft"
-            title="Link to Information About James"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/James_Croft.png"
-              alt="Picture of James Croft"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../matt-whetton"
-            title="Link to Information About Matt"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Matt_Whetton.png"
-              alt="Picture of Matt Whetton"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../ryan-yates"
-            title="Link to Information About Ryan"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Ryan_Yates.png"
-              alt="Picture of Ryan Yates"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../poornima-nayar"
-            title="Link to Information About Poornima"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Poornima_Nayar.png"
-              alt="Picture of Poornima Nayar"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../tom-morton"
-            title="Link to Information About Tom"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Tom_Morton.png"
-              alt="Picture of Tom Morton"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../kojo-hinson"
-            title="Link to Information About Kojo"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Kojo_Hinson.png"
-              alt="Picture of Kojo Hinson"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../stephen-jackson"
-            title="Link to Information About Stephen"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Stephen_Jackson.png"
-              alt="Picture of Stephen Jackson"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../riz-akmal-khan"
-            title="Link to Information About Riz"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Rizwana_Akmal_Khan.png"
-              alt="Picture of Rizwana Akmal Khan"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../leke-sholuade"
-            title="Link to Information About Leke"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Leke_Sholuade.jpg"
-              alt="Picture of Leke Sholuade"
-            />
-          </a>
-        </article>
-        <article className="speaker-grid-item">
-          <a
-            tabIndex="0"
-            href="../jennifer-mackown"
-            title="Link to Information About Jennifer"
-          >
-            <img
-              className="speaker-image"
-              src="/static/speakers/2021/Jen_Mackown.png"
-              alt="Picture of Jennifer Mackown"
-            />
-          </a>
-        </article>
+      <section className="speaker-grid">        
+        <SpeakerGridItemComponent speakername="Lex" pic="/static/speakers/2021/Lex_Lofthouse.png" link="../lex-lofthouse" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Connell" pic="/static/speakers/2021/Connell_W.png" link="../connell-w" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Lucy" pic="/static/speakers/2021/Lucy_Fletcher.png" link="../lucy-fletcher" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Simon" pic="/static/speakers/2021/Simon_Painter.png" link="../simon-painter" talkbrief=" " />
+
+        <SpeakerGridItemComponent speakername="Steve Collins" pic="/static/speakers/2021/steve_collins.jpg" link="../steve-collins" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Stephen" pic="/static/speakers/2021/Stephen_Haunts.jpg" link="../stephen-haunts" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Manu Magalhaes" pic="/static/speakers/2021/Manu_Magalhaes.png" link="../manu-magalhaes" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="James" pic="/static/speakers/2021/James_Croft.png" link="../james-croft" talkbrief=" " />
+
+        <SpeakerGridItemComponent speakername="Matt" pic="/static/speakers/2021/Matt_Whetton.png" link="../matt-whetton" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Ryan" pic="/static/speakers/2021/Ryan_Yates.png" link="../ryan-yates" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Poornima" pic="/static/speakers/2021/Poornima_Nayar.png" link="../poornima-nayar" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Tom" pic="/static/speakers/2021/Tom_Morton.png" link="../tom-morton" talkbrief=" " />
+
+        <SpeakerGridItemComponent speakername="Kojo Hinson" pic="/static/speakers/2021/Kojo_Hinson.png" link="../kojo-hinson" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Stephen Jackson" pic="/static/speakers/2021/Stephen_Jackson.png" link="../stephen-jackson" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Rizwana Akmal Khan" pic="/static/speakers/2021/Rizwana_Akmal_Khan.png" link="../riz-akmal-khan" talkbrief=" " />
+        <SpeakerGridItemComponent speakername="Leke Sholuade" pic="/static/speakers/2021/Leke_Sholuade.jpg" link="../leke-sholuade" talkbrief=" " />
+
+        <SpeakerGridItemComponent speakername="Jennifer Mackown" pic="/static/speakers/2021/Jen_Mackown.png" link="../jennifer-mackown" talkbrief=" " />
+
+    
+     
       </section>
 
       <style jsx>
@@ -284,7 +91,23 @@ export default function Speaker2021Component() {
           }
           .speaker-grid-item {
             min-width: 180px;
+            height: 180px;
+            overflow: hidden;
           }
+
+          .hover-pop{
+            width: 175px;
+            background-color:pink;
+            position: relative;
+            top: -10px;
+            margin-left: 5px;
+          }
+
+          .hover-pop.popped{
+            top: -145px;
+            padding:7px;
+          }
+
         `}
       </style>
     </Layout>
