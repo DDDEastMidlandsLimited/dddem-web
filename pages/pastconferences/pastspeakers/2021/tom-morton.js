ef4692535e753cb78c99143a8b83df827255061d
Jessica <me@jesswhite.co.uk>
2021-11-06 14:52:16 +0000

    Add Youtube Video to Toms page

--- pages/pastconferences/pastspeakers/2021/tom-morton.js
+++ pages/pastconferences/pastspeakers/2021/tom-morton.js
@@ -66,6 +66,32 @@ export default function TomMorton() {
       </section>
 
       <section>
+        <h2 tabIndex="0">Video from 2021</h2>
+        <br/>
+        <div className="video-frame">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/K1KYdtYnCns"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="center-link">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Tom: Youtube of Talk"
+            href="https://youtu.be/K1KYdtYnCns"
+          >
+            Link to Youtube Recording
+          </a>
+        </div>
+      </section>
+
+      <section>
         <h2 tabIndex="0">Links</h2>
         <li>
           <a
@@ -153,6 +179,13 @@ export default function TomMorton() {
             text-align: left;
             flex-basis: 0;
             flex-grow: 1;
+          }
+
+          .video-frame {
+            text-align: center;
+          }
+          .center-link {
+            text-align: center;
           }
 
           .contactButton {
