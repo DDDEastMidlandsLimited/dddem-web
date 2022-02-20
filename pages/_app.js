bf36358bad50210688f1b643ef20dbf72e7787ed
John Nolan <me@johnnolan.dev>
2022-02-20 07:15:34 +0000

    Upgrade to ECMAScript

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../theme/bootstrap.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
 
 export default MyApp;
