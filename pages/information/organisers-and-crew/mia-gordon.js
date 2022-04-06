6ea6d1788c182dce5d9ddcfe0633f4917573a0b5
Jessica <me@jesswhite.co.uk>
2022-04-06 19:16:17 +0100

    Add information about Mia

---
+++ pages/information/organisers-and-crew/mia-gordon.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+import Header from '../../../components/Header';
+import theme from '../../../theme/theme';
+import Head from 'next/head';
+import CrewBackButton from '../../../components/information/CrewBackButton';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faInstagram,
+  faTwitter,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faInstagram, faTwitter, faGithub, faLinkedin);
+
+export default function MiaGordon() {
+  return (
+    <Layout>
+      <Head>
+        <title>Mia Gordon</title>
+      </Head>
+      <Header
+        title={'Mia Gordon'}
+        banner="/static/banners/information/mia_banner.jpg"
+      />
+      <section>
+        <h1 tabIndex="0">Social Responsibility Manager</h1>
+      </section>
+      <section>
+        <img
+          src="/static/organisers/mia.jpg"
+          alt="Picture of Mia Gordon"
+          className="speakerpic"
+        />
+        <div>
+          <ul>
+            <b>Pronouns: </b> She/Her
+          </ul>
+        </div>
+        <br />
+      </section>
+      <section>
+        <h2 tabIndex="0">About Mia</h2>
+        <p tabIndex="0">
+          Mia is a developer, and joined the organisation committee
+          ready for the 2023 event.
+        </p>
+      </section>
+
+      <section>
+        <h2 tabIndex="0">Mia's Responsibilities</h2>
+        <p tabIndex="0">
+          Organising outreach. This can include working with
+          universities, boot camps, organisations such as Coding Black
+          Females. Helping us get a diverse range of speakers and
+          publicising great initiatives.
+        </p>
+      </section>
+
+      <section>
+        <CrewBackButton />
+      </section>
+
+      <style jsx>
+        {`
+          h1 {
+            text-align: center;
+          }
+          section {
+            max-width: ${theme.sizes.maxContentWidth};
+            padding: ${theme.sizes.contentPadding};
+            margin: auto;
+          }
+
+          .row:after {
+            content: '';
+            display: table;
+            clear: both;
+          }
+
+          .speakerpic {
+            margin-bottom: 5%;
+            width: 250px;
+            border-radius: 50%;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+          }
+
+          .iframe-container {
+            position: relative;
+            overflow: hidden;
+            padding-top: 56.25%;
+            margin: 10px 10px;
+          }
+          .iframe-container iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: 0;
+          }
+          ul {
+            display: flex;
+            margin: 0;
+            padding: 5px;
+            flex-flow: row;
+            flex-wrap: nowrap;
+            justify-content: center;
+          }
+          li {
+            margin: 0;
+            padding: 10px;
+            list-style: inside;
+            list-style-type: disc;
+            text-align: left;
+            flex-basis: 0;
+            flex-grow: 1;
+          }
+
+          .contactButton {
+            align: ${theme.palette.quaternary};
+            font-weight: bold;
+            font-size: ${theme.font.sizes.mobile.subtitle1};
+            flex-grow: 1;
+            border-radius: 12px;
+            padding: 10px 10px 10px 10px;
+            color: ${theme.palette.primary};
+            border: 2px solid ${theme.palette.primary};
+            background-color: ${theme.palette.light};
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2),
+              0 6px 20px 0 rgba(0, 0, 0, 0.19);
+          }
+          .contactButton a {
+            text-decoration: none;
+          }
+
+          .contactButton:hover {
+            background-color: ${theme.palette.primary};
+          }
+
+          .contactButton a:hover {
+            color: ${theme.palette.light};
+          }
+
+          @media only screen and (max-width: 1081px) {
+            section {
+              padding: 3% 12%;
+            }
+          }
+        `}
+      </style>
+    </Layout>
+  );
+}
