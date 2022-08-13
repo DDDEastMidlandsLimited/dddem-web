c67652f617d24a99e4b8025a755b04e908e08346
Jessica <me@jesswhite.co.uk>
2022-08-13 23:18:04 +0100

    Edit next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require('next-pwa');
+const glob = require('glob');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
@@ -12,4 +13,27 @@ module.exports = withPWA({
       allowFutureImage: true,
     },
   },
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId },
+  ) {
+    const pathMap = {};
+    glob.sync('pages/**/*.js').forEach((s) => {
+      if (s.indexOf('_') === -1) {
+        const path = s
+          .split(/(pages|\.)/)[2]
+          .replace(/^\/index$/, '/');
+        pathMap[path] = { page: path };
+      }
+    });
+    return pathMap;
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generateSiteMap.js');
+    }
+
+    return config;
+  }
 });
