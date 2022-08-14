0d3ac2a9f177c2173fa3ca058a9b5658b14aaf7e
Jessica <me@jesswhite.co.uk>
2022-08-14 16:28:12 +0100

    Remove WhereWhen

--- components/content/index.js
+++ components/content/index.js
@@ -1,6 +1,5 @@
 import CodeOfConduct from './CodeOfConduct';
 import Introduction from './Introduction';
-import WhereWhen from './WhereWhen';
 import Transport from './Transport';
 import Catering from './Catering';
 import Wifi from './Wifi';
