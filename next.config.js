e327b5d54a935baed27a3546759a5cd3bfb07db5
Jessica <me@jesswhite.co.uk>
2022-08-14 12:20:41 +0100

    Revert changes to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,42 +1,44 @@
 const withPWA = require('next-pwa');
 const glob = require('glob');
-const runtimeCaching = require('next-pwa/cache');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-  trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId },
-  ) {
-    const pathMap = {};
-    glob.sync('pages/**/*.js').forEach((s) => {
-      if (s.indexOf('_') === -1) {
-        const path = s
-          .split(/(pages|\.)/)[2]
-          .replace(/^\/index$/, '/');
-        pathMap[path] = { page: path };
-      }
-    });
-    return pathMap;
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
-  ) => {
-    if (isServer) {
-      require('./utils/generateSiteMap.js');
-    }
-
-    return config;
-  },
-  experimental: {
-    images: {
-      unoptimized: true,
-      allowFutureImage: true,
+module.exports = withPlugins(
+  [
+    {
+      experimental: {
+        images: {
+          unoptimized: true,
+          },
+          allowFutureImage: true,
+        },
+      },
+  ],
+  withPWA({
+    pwa: {
+      dest: 'public',
     },
-  },
-});
+    trailingSlash: true,
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId },
+    ) {
+      const pathMap = {};
+      glob.sync('pages/**/*.js').forEach((s) => {
+        if (s.indexOf('_') === -1) {
+          const path = s
+            .split(/(pages|\.)/)[2]
+            .replace(/^\/index$/, '/');
+          pathMap[path] = { page: path };
+        }
+      });
+      return pathMap;
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./utils/generateSiteMap.js');
+      }
+
+      return config;
+    },
+  }),
+);
