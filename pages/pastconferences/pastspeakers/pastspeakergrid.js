f7b3944172cc0b7b096d66ab926c3d68ac5471b4
Jessica <me@jesswhite.co.uk>
2022-08-16 14:45:56 +0100

    Add images to speaker grid

--- pages/pastconferences/pastspeakers/pastspeakergrid.js
+++ pages/pastconferences/pastspeakers/pastspeakergrid.js
@@ -27,115 +27,115 @@ export default function Speaker2021Component() {
       <section className="speaker-grid">
         <SpeakerGridItemComponent
           speakername="Tania Allard"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Tania_A.jpg"
           link="../2021/tania-allard"
           talkbrief="KEYNOTE: Open source for fun and for profit"
         />
       </section>
-{/*       <section className="speaker-grid">
+       <section className="speaker-grid">
         <SpeakerGridItemComponent
           speakername="Lex Lofthouse"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Lex_L.jpg"
           link="../2021/lex-lofthouse"
           talkbrief="Design for Developers"
         />
         <SpeakerGridItemComponent
           speakername="Connell"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Connell_W.jpg"
           link="../2021/connell-w"
           talkbrief="Onion Architecture with DDD and CQRS"
         />
         <SpeakerGridItemComponent
           speakername="Dylan Beattie"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Dylan_B.jpg"
           link="../2021/dylan-beattie"
           talkbrief="There's No Such Thing As Plain Text"
         />
         <SpeakerGridItemComponent
           speakername="Simon Painter"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Simon_P.jpg"
           link="../2021/simon-painter"
           talkbrief="SOLID Principles in 5 Nightmares"
         />
         <SpeakerGridItemComponent
           speakername="Steve Collins"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Steve_C.jpg"
           link="../2021/steve-collins"
           talkbrief="The [Source Code] Generation Game"
         />
         <SpeakerGridItemComponent
           speakername="Stephen Haunts"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Stephen_H.jpg"
           link="../2021/stephen-haunts"
           talkbrief="Hacking Humans"
         />
         <SpeakerGridItemComponent
           speakername="Manu Magalhaes"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Manu_M.jpg"
           link="../2021/manu-magalhaes"
           talkbrief="Nitty-Gitty: Master Git from the Inside"
         />
         <SpeakerGridItemComponent
           speakername="James Croft"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/James_C.jpg"
           link="../2021/james-croft"
           talkbrief="Should you write UI tests?"
         />
         <SpeakerGridItemComponent
           speakername="Matt Whetton"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/Matt_W.jpg"
           link="../2021/matt-whetton"
           talkbrief="We need to talk about technical debt"
         />
         <SpeakerGridItemComponent
           speakername="Clifford Agius"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Clifford_A.jpg"
           link="../2021/clifford-agius"
           talkbrief="3D printed Bionic Hand a little IOT and a Xamarin Mobile App"
         />
         <SpeakerGridItemComponent
           speakername="Poornima Nayar"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Poornima_N.jpg"
           link="../2021/poornima-nayar"
           talkbrief="REST, GraphQL and gRPC : A Comparison"
         />
         <SpeakerGridItemComponent
           speakername="Tom Morton"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Tom_M.jpg"
           link="../2021/tom-morton"
           talkbrief="Open Banking vs. DeFi; The technological future of finance?"
         />
         <SpeakerGridItemComponent
           speakername="Kojo Hinson"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Kojo_H.jpg"
           link="../2021/kojo-hinson"
           talkbrief="Embracing Skynet - An exploration of GPT-3 and its potential applications"
         />
         <SpeakerGridItemComponent
           speakername="Stephen Jackson"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Stephen_J.jpg"
           link="../2021/stephen-jackson"
           talkbrief="Senior By Default"
         />
         <SpeakerGridItemComponent
           speakername="Rizwana Akmal Khan"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Rizwana_A.jpg"
           link="../2021/riz-akmal-khan"
           talkbrief="Good Writers Become Better Developers"
         />
         <SpeakerGridItemComponent
           speakername="Leke Sholuade"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Leke_S.jpg"
           link="../2021/leke-sholuade"
           talkbrief="Why do we need a Black Valley"
         />
         <SpeakerGridItemComponent
           speakername="Jennifer Mackown"
-          pic="/static/speakers/TBC.png"
+          pic="/static/speakers/2021/Jen_M.jpg"
           link="../2021/jennifer-mackown"
           talkbrief="How to ruin kid's games with machine learning"
         />
-      </section> */}
+      </section>
       <section>
         <h2>2019</h2>
       </section>
