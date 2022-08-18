0d791ae4707ca5d6eec3184e430df5e938a6d2d4
Jessica <me@jesswhite.co.uk>
2022-08-18 14:18:24 +0100

    Fix iFrame fields

--- pages/2023/sponsor.js
+++ pages/2023/sponsor.js
@@ -58,9 +58,9 @@ export default function Sponsor() {
             height="315"
             src="https://www.youtube.com/embed/e0ETxkb_gSk"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
             layout="responsive"
             display="block"
           ></iframe>
