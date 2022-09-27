694aed9b99f9d4793bab4566278167fc495bfdcf
John Nolan <me@johnnolan.dev>
2022-09-27 18:17:30 +0100

    Base layout and venuebutton

--- components/VenueButton.js
+++ components/VenueButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import theme from '../theme/theme';
 
 export default function VenueButton() {
   return (
@@ -14,38 +13,6 @@ export default function VenueButton() {
           View Printable Travel Guide
         </a>
       </button>
-      <style jsx>
-        {`
-          .venueButtonContainer {
-            align: center;
-          }
-
-          .venueButton {
-            align: ${theme.palette.quaternary};
-            font-weight: bold;
-            font-size: ${theme.font.sizes.mobile.subtitle1};
-            flex-grow: 1;
-            border-radius: 12px;
-            padding: 10px 10px 10px 10px;
-            color: ${theme.palette.primary};
-            border: 2px solid ${theme.palette.primary};
-            background-color: ${theme.palette.light};
-            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2),
-              0 6px 20px 0 rgba(0, 0, 0, 0.19);
-          }
-          .venueButton a {
-            text-decoration: none;
-          }
-
-          .venueButton:hover {
-            background-color: ${theme.palette.primary};
-          }
-
-          .venueButton a:hover {
-            color: ${theme.palette.light};
-          }
-        `}
-      </style>
     </div>
   );
 }
