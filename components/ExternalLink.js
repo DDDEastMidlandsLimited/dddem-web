cdf6f8c292089c6492f5cd6480c4faaa3464f6bd
John Nolan <me@johnnolan.dev>
2022-09-27 18:35:09 +0100

    ExternalLink, FullImage and Footer

--- components/ExternalLink.js
+++ components/ExternalLink.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from 'next/router';
-import theme from '../theme/theme';
 import { logEvent } from '../utils/analytics';
 
 function ExternalLink({
@@ -16,33 +15,14 @@ function ExternalLink({
   };
 
   return (
-    <a href={href} onClick={handleClick} target={target} rel={rel}>
+    <a
+      className="externalLink"
+      href={href}
+      onClick={handleClick}
+      target={target}
+      rel={rel}
+    >
       {children}
-      <style jsx>
-        {`
-          a {
-            text-decoration: none;
-            color: ${theme.palette.light};
-            display: block;
-            padding: 0.75em 15px;
-            line-height: 1em;
-            font-size: 1em;
-          }
-
-          a:hover {
-            color: ${theme.palette.light};
-            text-decoration: underline;
-          }
-
-          @media (min-width: 768px) {
-            a {
-              color: ${theme.palette.primary};
-              border: 0 !important;
-              border-bottom: none;
-            }
-          }
-        `}
-      </style>
     </a>
   );
 }
