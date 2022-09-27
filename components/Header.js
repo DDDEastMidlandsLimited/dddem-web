b8cf573f67134efd99e558199504536531200929
John Nolan <me@johnnolan.dev>
2022-09-27 18:28:00 +0100

    Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import theme from '../theme/theme';
 
 export default function Header({ title, banner }) {
   return (
-    <section>
-      <div className="background"></div>
+    <section className="header">
+      <div
+        className="background"
+        style={{ backgroundImage: `url("${banner}")` }}
+      ></div>
 
       <div className="logo">
         <img
@@ -18,61 +20,7 @@ export default function Header({ title, banner }) {
         <h1 tabIndex="0">{title}</h1>
       </div>
 
-      <style jsx>
-        {`
-          h1 {
-            font-size: ${theme.font.sizes.mobile.headline1};
-            color: ${theme.palette.primary};
-          }
-          h3 {
-            color: ${theme.palette.light};
-          }
-          h3 a {
-            color: ${theme.palette.light};
-          }
-          .background {
-            width: 100vw;
-            background-image: url(${banner});
-            background-color: ${theme.palette.primary};
-            height: 300px;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-          }
-          .logo {
-            text-align: center;
-          }
-          .image {
-            margin-top: -80px;
-            padding: 20px 30px;
-            background-color: #1a7d87;
-            width: 250px;
-            border-radius: ${theme.sizes.borderRadiusMobile};
-            background-color: ${theme.palette.primary};
-          }
-          .covid {
-            text-align: center;
-            padding: 10px 10px;
-            background: ${theme.palette.secondary};
-          }
-
-          @media (min-width: 600px) {
-            h1 {
-              font-size: ${theme.font.sizes.desktop.headline1};
-              color: ${theme.palette.primary};
-            }
-            .background {
-              height: 350px;
-            }
-            .image {
-              margin-top: -80px;
-              padding: 20px 30px;
-              background-color: #1a7d87;
-              width: 250px;
-            }
-          }
-        `}
-      </style>
+      <style jsx>{``}</style>
     </section>
   );
 }
