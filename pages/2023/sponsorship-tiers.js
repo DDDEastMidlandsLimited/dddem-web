cac1179ffea53a5b40f6281e45a14de3a5b47b88
Jessica <me@jesswhite.co.uk>
2022-09-27 20:30:41 +0100

    Fix build error

--- pages/2023/sponsorship-tiers.js
+++ pages/2023/sponsorship-tiers.js
@@ -19,7 +19,7 @@ export default function SponsorshipPack() {
             height="315"
             src="https://www.youtube.com/embed/1ATh2PbQx1Q"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
