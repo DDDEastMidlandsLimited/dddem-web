243280a518f118064ad18ff2e76a8614b82b45c5
John Nolan <me@johnnolan.dev>
2022-09-27 17:20:45 +0100

    Index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import theme from '../theme/theme';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
 import ImportantDatesList from '../components/home/ImportantDatesList';
@@ -12,33 +11,32 @@ export default function Index() {
     <Layout>
       <Header banner="/static/banners/homepage.jpg" />
 
-      <h3>Developer! Developer! Developer! East Midlands</h3>
-
-      <section>
-        <div className="row">
-          <p tabIndex="0">
-            DDD East Midlands is an inclusive, not-for-profit
-            technology conference happening on <b>7th October 2023</b>{' '}
-            that celebrates the unique tech, talent and companies that
-            the East Midlands has to offer. This event is run by
-            community members to promote collaboration and the amazing
-            tech community that has already grown in the East
-            Midlands.
-          </p>
-        </div>
+      <section className="content-section">
+        <h3 className="highlight">
+          Developer! Developer! Developer! East Midlands
+        </h3>
       </section>
 
-      <section>
-        <div>
-          <Image
-            className="pageimage"
-            src="/static/pageimage/happy.jpg"
-            alt={`Image of the attendees of DDD East Midlands Conference 2019 seated in red auditorium seating.`}
-            height="85"
-            width="200"
-            layout="responsive"
-          />
-        </div>
+      <section className="content-section">
+        <p tabIndex="0">
+          DDD East Midlands is an inclusive, not-for-profit technology
+          conference happening on <b>7th October 2023</b> that
+          celebrates the unique tech, talent and companies that the
+          East Midlands has to offer. This event is run by community
+          members to promote collaboration and the amazing tech
+          community that has already grown in the East Midlands.
+        </p>
+      </section>
+
+      <section className="content-section">
+        <Image
+          className="pageimage"
+          src="/static/pageimage/happy.jpg"
+          alt={`Image of the attendees of DDD East Midlands Conference 2019 seated in red auditorium seating.`}
+          height="85"
+          width="200"
+          layout="responsive"
+        />
       </section>
 
       {/* Gold Sponsors */}
@@ -50,13 +48,13 @@ export default function Index() {
       </section> */}
 
       {/* Silver Sponsors */}
-      <section>
+      <section className="content-section">
         <h2>Sponsors</h2>
         <SponsorButton />
       </section>
 
       {/* Important Dates */}
-      <section>
+      <section className="content-section">
         <br />
         <br />
         <h2 tabIndex="0">Important Dates</h2>
@@ -64,7 +62,7 @@ export default function Index() {
       </section>
 
       {/* Principles */}
-      <section>
+      <section className="content-section">
         <br />
         <h2 tabIndex="0">Principles</h2>
         <p tabIndex="0">
@@ -90,87 +88,6 @@ export default function Index() {
         </ul>
         <br />
       </section>
-
-      <style jsx>
-        {`
-          section {
-            max-width: ${theme.sizes.maxContentWidth};
-            padding: ${theme.sizes.contentPadding};
-            margin: auto;
-          }
-
-          h2 {
-            text-align: center;
-          }
-          h3 {
-            text-align: center;
-            color: ${theme.palette.primary};
-          }
-
-          .row {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            width: 100%;
-          }
-          .column {
-            display: flex;
-            flex-direction: column;
-            flex-basis: 80%;
-            flex: 1;
-            padding: 5px 5px;
-            margin-left: 15px;
-          }
-
-          .subheader {
-            margin: 0px;
-            padding-bottom: 15px;
-          }
-
-          .banner {
-            text-align: center;
-            padding: 5px 5px;
-            background: ${theme.palette.primary};
-            color: ${theme.palette.light};
-          }
-          .banner a {
-            text-align: center;
-            color: ${theme.palette.light};
-          }
-
-          .platinum {
-            text-align: center;
-            padding: 5px 5px;
-          }
-
-          .pageimage {
-            object-fit: contain;
-            text-align: center;
-            max-width: 100%;
-            padding: 5px 5px;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-          }
-
-          .info-text {
-            padding: 1% 5%;
-          }
-
-          .silver {
-            margin: auto;
-            width: 35%;
-          }
-
-          .infolink {
-            margin: 10px;
-          }
-          .silver {
-            text-align: center;
-            width: 70%;
-          }
-        `}
-      </style>
     </Layout>
   );
 }
