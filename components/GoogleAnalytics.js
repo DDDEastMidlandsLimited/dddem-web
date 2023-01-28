8aa97a7b7112c436aea271466b4afc94d323cf10
Jessica White <me@jesswhite.co.uk>
2023-01-28 15:40:05 +0000

    run prettier

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { initGA, logPageView } from '../utils/analytics';
 
-const isBrowser = () => typeof window !== "undefined";
+const isBrowser = () => typeof window !== 'undefined';
 
-if (isBrowser){
+if (isBrowser) {
   if (!window.GA_INITIALIZED) {
     initGA();
     window.GA_INITIALIZED = true;
