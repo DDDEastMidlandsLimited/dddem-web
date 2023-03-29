fbc6f09789d96904722b4bf1be5d9d9783f1ca0b
Jessica White <me@jesswhite.co.uk>
2023-03-29 19:41:22 +0100

    Enhancement: Add NextTechGirls as a partner (#1097)

--- data/sponsors/partners.js
+++ data/sponsors/partners.js
@@ -1,9 +1,15 @@
 const partners = [
   {
     id: 1,
-    name: 'Motorpoint',
+    name: 'Muslamic Makers',
     image: '/static/partners/muslamic-makers.png',
     link: 'https://muslamicmakers.com/',
+  },
+  {
+    id: 2,
+    name: 'Next Tech Girls',
+    image: '/static/partners/next-tech-girls.png',
+    link: 'https://nexttechgirls.com/',
   },
 ];
 
