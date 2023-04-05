a3c785d411a86f1e427885eaecd5e20ca0b03c0b
Jessica White <me@jesswhite.co.uk>
2023-04-05 11:33:07 +0100

    Enhancement: Add Cronofy as sponsor (#1108)
    * Add Cronofy
    
    * Optimised images with calibre/image-actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- data/sponsors/gold.js
+++ data/sponsors/gold.js
@@ -1,6 +1,9 @@
 const motorpointdesc =
   '<h1>Motorpoint</h1><p>With its HQ in Derby, Motorpoint has grown to become the UK’s largest independent retailer of nearly new cars and vans. Motorpoint was established in 1998 and employs over 800 people across its store network, preparation centres and head office.</p><p>Marking its 25th Anniversary this year, Motorpoint has 19 retail stores across England, Scotland and Wales, with its 20th store set to open in Ipswich in May 2023.</p><p>Alongside the continued expansion of its retail store footprint, Motorpoint is also investing heavily in its technology capability utilising the latest serverless cloud technologies and industry innovations to enhance its online store experience, streamline its customer journey and continuously improve its logistical operations.</p><p>As part of that investment, Motorpoint is looking to attract the very best talent in the tech industry, to fulfil a wide variety of software engineering and technology roles. Known in the vehicle retail industry for its award-winning company culture, team building and career advancement, Motorpoint is now looking to grow its reputation as a technology leader in the vehicle retail sector.</p><p>Why not head over to <a href="https://motorpoint.co.uk" target="_blank" category="notable-companies" eventType="Motorpoint">https://motorpoint.co.uk</a> and take a look at what we do. If you are looking for your next career move, we are always keen to hear from passionate individuals. You can find our current career opportunities at <a href="https://careers.motorpoint.co.uk/jobs?department=Technology" target="blank" category="notable-companies" eventType="Motorpoint">https://careers.motorpoint.co.uk/jobs?department=Technology</a> or get in touch with us at engineering@motorpoint.co.uk</p>';
 
+const cronofydesc =
+  '<h1>Cronofy</h1><p>Cronofy is the worlds most powerful scheduling tool used by tens of thousands of businesses. We are the technology powering scheduling for market leading firms like Indeed, GoDaddy and Personio.With a Nottingham HQ, Cronofy has offices in London and Amsterdam, while supporting customers all over the world. Our workforce is currently 30 strong (and rapidly growing!), covering the UK, Netherlands, Spain and France.</p><p>We are a product-focused company led by a successful serial entrepreneur. We are at that exciting stage where every person that joins the team has a significant hand in our success, while having the opportunity to rapidly advance their careers along the way.</p>';
+
 const gold = [
   {
     id: 1,
@@ -8,6 +11,13 @@ const gold = [
     image: '/static/sponsors/Motorpoint.png',
     link: 'https://careers.motorpoint.co.uk/',
     description: motorpointdesc,
+  },
+  {
+    id: 2,
+    name: 'Cronofy',
+    image: '/static/sponsors/Cronofy.png',
+    link: 'https://www.cronofy.com/careers',
+    description: cronofydesc,
   },
 ];
 
