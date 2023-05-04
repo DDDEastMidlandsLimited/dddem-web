c6975f09626f504ece30475344718753f54dbe54
Jessica White <me@jesswhite.co.uk>
2023-05-04 14:36:48 +0100

    Enhancement: Add codebar to Notable Companies (#1153)
    * Add codebar to Notable Companies
    
    * Optimised images with calibre/image-actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- data/sponsors/partners.js
+++ data/sponsors/partners.js
@@ -11,6 +11,12 @@ const partners = [
     image: '/static/partners/next-tech-girls.png',
     link: 'https://nexttechgirls.com/',
   },
+  {
+    id: 3,
+    name: 'Codebar',
+    image: '/static/partners/codebar.png',
+    link: 'https://codebar.io/birmingham',
+  },
 ];
 
 export default partners;
