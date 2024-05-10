2f6f876b09891732f2a546afb8883b2dd0467d2e
Jessica Brentnall <mrsjessicabrentnall@gmail.com>
2024-05-10 16:45:44 +0100

    [Snyk] Security upgrade next from 13.5.6 to 14.1.1 (#1501)
    * fix: package.json & package-lock.json to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-NEXT-6828457
    
    * fix npm export error
    
    ---------
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

---
+++ out/_next/static/chunks/2762.8a575ba68f1dc315.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2762],{2762:function(n,u,e){e.r(u),e.d(u,{default:function(){return c}}),e(7294);var t=e(3292),I=e(5893);function c(){return(0,I.jsx)("span",{})}window.GA_INITIALIZED||((0,t._k)(),window.GA_INITIALIZED=!0),(0,t.Nc)()}}]);
