1e5e1a998869498e9740c3130b933572d37d88d3
Jessica Brentnall <mrsjessicabrentnall@gmail.com>
2026-04-12 21:34:29 +0100

    Edit footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,39 +9,32 @@ export default function Footer() {
     <footer className="footer">
       <div className="row">
         <div className="col-3">
-          <Link
-            href="/code-of-conduct"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <Link href="/code-of-conduct">
             Code of Conduct
           </Link>
         </div>
         <div className="col-3">
           <Link
             href="https://www.youtube.com/channel/UC5TNH43dpYqmw3ggv9OGIKw"
-            prefetch={false}
             target="_blank"
-            aria-label="youtube"
             rel="noopener noreferrer"
+            aria-label="youtube"
           >
             <FontAwesomeIcon icon={brands('youtube')} size="sm" />
           </Link>
           <Link
             href="https://github.com/DDDEastMidlandsLimited/dddem-web"
-            prefetch={false}
             target="_blank"
-            aria-label="GitHub"
             rel="noopener noreferrer"
+            aria-label="GitHub"
           >
             <FontAwesomeIcon icon={brands('github')} size="sm" />
           </Link>
           <Link
             href="https://www.linkedin.com/company/ddd-east-midlands-limited/"
-            prefetch={false}
             target="_blank"
-            aria-label="LinkedIn"
             rel="noopener noreferrer"
+            aria-label="LinkedIn"
           >
             <FontAwesomeIcon icon={brands('linkedin')} size="sm" />
           </Link>
